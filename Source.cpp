b198180a376212ce8037f880115018d5cfd7a7ab
DESKTOP-EAHJTPT\AlexLip <lipchaksasha505@gmail.com>
2023-11-19 23:22:39 +0300

    add load config and switch custom/default python path

--- Source.cpp
+++ Source.cpp
@@ -4,51 +4,74 @@
 #include <cstdlib>
 #include <regex>
 #include <windows.h> 
-//#include "dependencies/include/nlohmann/json.hpp"
+#include "dependencies/include/nlohmann/json.hpp"
 
 void runProgram(const std::string& programPath, const std::string& filePath, const std::string& parameters = "") {
-    std::string command = programPath + " " + filePath + " " + parameters;
+    std::string command = programPath + " " + filePath;
     if (parameters != "")
     {
         command += " " + parameters;
     }
-
     system(command.c_str());
 }
 
 
+nlohmann::json data(std::string file = "")
+{
 
+    std::ifstream ifs(file);
+    nlohmann::json jf = nlohmann::json::parse(ifs);
+    return jf;
+}
 
 
 int main(int argc, char* argv[]) {
-    //добавить проверку python программы
-    /*
-	std::string filePath = "C:/Users/AlexLip/Desktop/6) UE4.Coding.rar";
-
-    std::ifstream file(filePath);
-    if (!file) {
-        throw std::runtime_error("Error: File not found at " + filePath);
-    }
-     */
 
 
     ShowWindow(GetConsoleWindow(), SW_HIDE);
 
     std::string selfPath(argv[0]);
-    //std::cout << selfPath << std::endl;
     std::string mainPython = std::regex_replace(selfPath, std::regex("GSPMWindows.exe"), "main.py");
-    //std::cout << mainPython << std::endl;
+    std::string configFile = std::regex_replace(selfPath, std::regex("GSPMWindows.exe"), "/Settings/Configs/Python.json");
+    std::string pythonPath;
 
+    nlohmann::json config = data(configFile);
+
+    if (config["is custom python"] == true)
+    {
+        pythonPath = config["cutsom python path"];
+        std::ifstream file(pythonPath);
+        if (!file) {
+            pythonPath = config["default python path"];
+            std::ifstream file(pythonPath);
+            if (!file)
+            {
+                std::string customPython = config["cutsom python path"];
+                std::string defaultPython = config["default python path"];
+                throw std::runtime_error("Error: Cutsom python path not found: " + customPython + ", and default python path not found: " + defaultPython);
+            }
+        }
+    }
+    else
+    {
+        pythonPath = config["default python path"];
+        std::ifstream file(pythonPath);
+        if (!file)
+        {
+            std::string customPython = config["cutsom python path"];
+            std::string defaultPython = config["default python path"];
+            throw std::runtime_error("Error: Cutsom python path not found: " + customPython + ", and default python path not found: " + defaultPython);
+        }
+    }
 
 
     if (argc < 2) {
-        runProgram("C:/Python3/pythonw.exe", mainPython);
+        runProgram(pythonPath, mainPython);
     }
     else
     {
         std::string parameters = argv[1];
-        //std::cout << parameters << std::endl;
-        runProgram("C:/Python3/pythonw.exe", mainPython, parameters);
+        runProgram(pythonPath, mainPython, parameters);
     }
     return 0;
 
