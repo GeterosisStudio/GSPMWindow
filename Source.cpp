e89f1e22189b4e0fa9b4700db734fdbf848a74d0
DESKTOP-EAHJTPT\AlexLip <lipchaksasha505@gmail.com>
2023-11-20 00:56:06 +0300

    refact

--- Source.cpp
+++ Source.cpp
@@ -15,7 +15,6 @@ void runProgram(const std::string& programPath, const std::string& filePath, con
     system(command.c_str());
 }
 
-
 nlohmann::json data(std::string file = "")
 {
 
@@ -24,17 +23,11 @@ nlohmann::json data(std::string file = "")
     return jf;
 }
 
+std::string getPythonPath(std::string selfPath)
 
-int main(int argc, char* argv[]) {
-
-
-    ShowWindow(GetConsoleWindow(), SW_HIDE);
-
-    std::string selfPath(argv[0]);
-    std::string mainPython = std::regex_replace(selfPath, std::regex("GSPMWindows.exe"), "main.py");
-    std::string configFile = std::regex_replace(selfPath, std::regex("GSPMWindows.exe"), "/Settings/Configs/Python.json");
+{
+    std::string configFile = std::regex_replace(selfPath, std::regex("GSPMWindows.exe"), "Settings/Configs/Python.json");
     std::string pythonPath;
-
     nlohmann::json config = data(configFile);
 
     if (config["is custom python"] == true)
@@ -58,12 +51,25 @@ int main(int argc, char* argv[]) {
         std::ifstream file(pythonPath);
         if (!file)
         {
-            std::string customPython = config["cutsom python path"];
-            std::string defaultPython = config["default python path"];
-            throw std::runtime_error("Error: Cutsom python path not found: " + customPython + ", and default python path not found: " + defaultPython);
+            pythonPath = config["cutsom python path"];
+            std::ifstream file(pythonPath);
+            if (!file)
+            {
+                std::string customPython = config["cutsom python path"];
+                std::string defaultPython = config["default python path"];
+                throw std::runtime_error("Error: Default python path not found: " + defaultPython + ", and Cutsom python path not found: " + customPython);
+            }
         }
     }
+    return pythonPath;
+}
 
+int main(int argc, char* argv[])
+{
+    ShowWindow(GetConsoleWindow(), SW_HIDE);
+
+    std::string pythonPath = getPythonPath(argv[0]);
+    std::string mainPython = std::regex_replace(argv[0], std::regex("GSPMWindows.exe"), "main.py");
 
     if (argc < 2) {
         runProgram(pythonPath, mainPython);
@@ -76,4 +82,3 @@ int main(int argc, char* argv[]) {
     return 0;
 
 }
-
